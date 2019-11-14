67a0ded4c0080b40e0513b265c1edb07b37d5ffa
Adrien Thiery <adrien.thiery@osedea.com>
2019-11-14 11:55:41 +0100

    Making it work with latest RN version (0.64.1)

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
-# My Plugin
+# Ignite SplashScreen
 
-This is where you would put an intro to your plugin, and maybe a few examples of usage. 
+This plugin helps you add a Splash Screen to your React Native app to:
+* Hide the fact that we are loading JS
+* Do stuff to get the app ready
+* Decide in JS when to hide the splashscreen
 
-## Example
+This is setuping the [react-native-splash-screen](https://github.com/crazycodeboy/react-native-splash-screen) package and setting some defaults.
 
-```js
-console.log("Hello World")
+Please refer to the [README.md](https://github.com/crazycodeboy/react-native-splash-screen) for more precision.
+
+We also recommend checking [react-native-make](https://github.com/bamlab/react-native-make) to set your custom splash image in one command.
+
+## Adding the plugin
+
+```sh
+ignite add splashscreen
 ```
+
+## Removing the plugin
+
+```sh
+ignite remove splashscreen
+```
+
+## Compatibility
+
+This has been tested with React Native [0.61.4](https://github.com/facebook/react-native/releases/tag/v0.61.4).
+
+## Contributing
+
+Please open an issue for any compatibility issues, or provide better interactivity/compatibility through opening a PR! 🙂
 
--- plugin.js
+++ plugin.js
@@ -1,30 +1,73 @@
 // Ignite CLI plugin for Splashscreen
 // ----------------------------------------------------------------------------
 
-const NPM_MODULE_NAME = 'react-native-MODULENAME'
-const NPM_MODULE_VERSION = '0.0.1'
+const NPM_MODULE_NAME = 'react-native-splash-screen'
+const NPM_MODULE_VERSION = '~3.2.0'
 
-// const PLUGIN_PATH = __dirname
-// const APP_PATH = process.cwd()
-
+const PLUGIN_PATH = __dirname
+const APP_PATH = process.cwd()
 
 const add = async function (toolbox) {
   // Learn more about toolbox: https://infinitered.github.io/gluegun/#/toolbox-api.md
-  const { ignite } = toolbox
+  const { ignite, print, filesystem, system } = toolbox
+  const packageJSON = require(`${APP_PATH}/package.json`)
+  const igniteJSON = require(`${APP_PATH}/ignite/ignite.json`)
 
   // install an NPM module and link it
-  await ignite.addModule(NPM_MODULE_NAME, { link: true, version: NPM_MODULE_VERSION })
+  await ignite.addModule(NPM_MODULE_NAME, { link: packageJSON.dependencies['react-native'] < '0.60.0', version: NPM_MODULE_VERSION })
+  await system.run('pod install', { cwd: `${APP_PATH}/ios` });
 
-  // Example of copying templates/Splashscreen to app/ignite-splashscreen
-  // if (!toolbox.filesystem.exists(`${APP_PATH}/app/ignite-splashscreen`)) {
-  //   toolbox.filesystem.copy(`${PLUGIN_PATH}/templates/ignite-splashscreen`, `${APP_PATH}/app/ignite-splashscreen`)
-  // }
+  // iOS install
+  ignite.patchInFile(`${APP_PATH}/ios/${packageJSON.name}/AppDelegate.m`, {
+    insert: `#import "RNSplashScreen.h"`,
+    after: `#import <React/RCTRootView.h>`
+  });
+  ignite.patchInFile(`${APP_PATH}/ios/${packageJSON.name}/AppDelegate.m`, {
+    insert: `    [RNSplashScreen show];\n`,
+    before: `return YES;`
+  });
+  // Android install
+  ignite.patchInFile(`${APP_PATH}/android/app/src/main/java/com/${packageJSON.name.toLowerCase()}/MainActivity.java`, {
+    insert: `import android.os.Bundle;`,
+    before: `import com.facebook.react.ReactActivity;`
+  });
+  ignite.patchInFile(`${APP_PATH}/android/app/src/main/java/com/${packageJSON.name.toLowerCase()}/MainActivity.java`, {
+    insert: `import org.devio.rn.splashscreen.SplashScreen;\n`,
+    after: `import com.facebook.react.ReactActivity;`
+  });
+  ignite.patchInFile(`${APP_PATH}/android/app/src/main/java/com/${packageJSON.name.toLowerCase()}/MainActivity.java`, {
+    insert: `  @Override\n  protected void onCreate(Bundle savedInstanceState) {\n    SplashScreen.show(this);\n    super.onCreate(savedInstanceState);\n  }\n`,
+    after: `public class MainActivity extends ReactActivity {`
+  });
+  filesystem.dir(`${APP_PATH}/android/app/src/main/res/layout`);
+  filesystem.copy(`${PLUGIN_PATH}/launch_screen.xml`, `${APP_PATH}/android/app/src/main/res/layout/launch_screen.xml`);
+  filesystem.copy(`${PLUGIN_PATH}/colors.xml`, `${APP_PATH}/android/app/src/main/res/values/colors.xml`);
+  filesystem.dir(`${APP_PATH}/android/app/src/main/res/drawable-mdpi`);
+  filesystem.dir(`${APP_PATH}/android/app/src/main/res/drawable-hdpi`);
+  filesystem.dir(`${APP_PATH}/android/app/src/main/res/drawable-xhdpi`);
+  filesystem.dir(`${APP_PATH}/android/app/src/main/res/drawable-xxhdpi`);
+  filesystem.dir(`${APP_PATH}/android/app/src/main/res/drawable-xxxhdpi`);
+  filesystem.copy(`${APP_PATH}/android/app/src/main/res/mipmap-mdpi/ic_launcher_round.png`, `${APP_PATH}/android/app/src/main/res/drawable-mdpi/launch_screen.png`);
+  filesystem.copy(`${APP_PATH}/android/app/src/main/res/mipmap-hdpi/ic_launcher_round.png`, `${APP_PATH}/android/app/src/main/res/drawable-hdpi/launch_screen.png`);
+  filesystem.copy(`${APP_PATH}/android/app/src/main/res/mipmap-xhdpi/ic_launcher_round.png`, `${APP_PATH}/android/app/src/main/res/drawable-xhdpi/launch_screen.png`);
+  filesystem.copy(`${APP_PATH}/android/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png`, `${APP_PATH}/android/app/src/main/res/drawable-xxhdpi/launch_screen.png`);
+  filesystem.copy(`${APP_PATH}/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png`, `${APP_PATH}/android/app/src/main/res/drawable-xxxhdpi/launch_screen.png`);
 
-  // Example of patching a file
-  // ignite.patchInFile(`${APP_PATH}/app/config/app-config.js`, {
-  //   insert: `import '../ignite-splashscreen/ignite-splashscreen'\n`,
-  //   before: `export default {`
-  // })
+  if (igniteJSON.boilerplate === 'osedea-react-native-boilerplate') {
+    ignite.patchInFile(`${APP_PATH}/app/index.tsx`, {
+        insert: `import SplashScreen from 'react-native-splash-screen';\n`,
+        after: `import React, { Component } from 'react';`
+    });
+    ignite.patchInFile(`${APP_PATH}/app/index.tsx`, {
+        insert: `    componentDidMount() {\n        SplashScreen.hide();\n    }\n`,
+        after: `class Root extends Component {`
+    });
+    await ignite.addModule('@bam.tech/react-native-make', { link: false, dev: true, version: '~1.0.3' })
+  } else {
+      print.info('Well done! You just need to hide the native splash screen in your JS code now!\n\nimport SplashScreen from \'react-native-splash-screen\';\n\nSplashScreen.hide();');
+  }
+  print.info('We highly recommend you to check out https://github.com/bamlab/react-native-make/blob/master/docs/set-splash.md to customize your splashscreen!');
+  print.info('Also, for more customization on Android, especially, see https://github.com/crazycodeboy/react-native-splash-screen');
 }
 
 /**
@@ -32,21 +75,50 @@ const add = async function (toolbox) {
  */
 const remove = async function (toolbox) {
   // Learn more about toolbox: https://infinitered.github.io/gluegun/#/toolbox-api.md
-  const { ignite } = toolbox
+  const { ignite, print, filesystem } = toolbox
+  const packageJSON = require(`${APP_PATH}/package.json`)
+  const igniteJSON = require(`${APP_PATH}/ignite/ignite.json`)
 
   // remove the npm module and unlink it
-  await ignite.removeModule(NPM_MODULE_NAME, { unlink: true })
+  await ignite.removeModule(NPM_MODULE_NAME, { unlink: packageJSON.dependencies['react-native'] < '0.60.0' })
+  await toolbox.system.run('pod install', { cwd: `${APP_PATH}/ios` })
 
-  // Example of removing app/Splashscreen folder
-  // const removeignite-splashscreen = await toolbox.prompt.confirm(
-  //   'Do you want to remove app/ignite-splashscreen?'
-  // )
-  // if (removeignite-splashscreen) { toolbox.filesystem.remove(`${APP_PATH}/app/ignite-splashscreen`) }
+  // iOS install
+  ignite.patchInFile(`${APP_PATH}/ios/${packageJSON.name}/AppDelegate.m`, {
+    delete: `#import "RNSplashScreen.h"`,
+  });
+  ignite.patchInFile(`${APP_PATH}/ios/${packageJSON.name}/AppDelegate.m`, {
+    delete: `    [RNSplashScreen show];\n`,
+  });
+  // Android install
+  ignite.patchInFile(`${APP_PATH}/android/app/src/main/java/com/${packageJSON.name.toLowerCase()}/MainActivity.java`, {
+    delete: `import android.os.Bundle;`,
+  });
+  ignite.patchInFile(`${APP_PATH}/android/app/src/main/java/com/${packageJSON.name.toLowerCase()}/MainActivity.java`, {
+    delete: `import org.devio.rn.splashscreen.SplashScreen;\n`,
+  });
+  ignite.patchInFile(`${APP_PATH}/android/app/src/main/java/com/${packageJSON.name.toLowerCase()}/MainActivity.java`, {
+    delete: `  @Override\n  protected void onCreate(Bundle savedInstanceState) {\n    SplashScreen.show(this);\n    super.onCreate(savedInstanceState);\n  }\n`,
+  });
+  filesystem.remove(`${APP_PATH}/android/app/src/main/res/layout`);
+  filesystem.remove(`${APP_PATH}/android/app/src/main/res/values/colors.xml`);
+  filesystem.remove(`${APP_PATH}/android/app/src/main/res/drawable-mdpi`);
+  filesystem.remove(`${APP_PATH}/android/app/src/main/res/drawable-hdpi`);
+  filesystem.remove(`${APP_PATH}/android/app/src/main/res/drawable-xhdpi`);
+  filesystem.remove(`${APP_PATH}/android/app/src/main/res/drawable-xxhdpi`);
+  filesystem.remove(`${APP_PATH}/android/app/src/main/res/drawable-xxxhdpi`);
 
-  // Example of unpatching a file
-  // ignite.patchInFile(`${APP_PATH}/app/config/app-config.js`, {
-  //   delete: `import '../ignite-splashscreen/ignite-splashscreen'\n`
-  // )
+  if (igniteJSON.boilerplate === 'osedea-react-native-boilerplate') {
+    ignite.patchInFile(`${APP_PATH}/app/index.tsx`, {
+        delete: `import SplashScreen from 'react-native-splash-screen';\n`,
+    });
+    ignite.patchInFile(`${APP_PATH}/app/index.tsx`, {
+        delete: `    componentDidMount() {\n        SplashScreen.hide();\n    }\n`,
+    });
+    await ignite.removeModule('@bam.tech/react-native-make', { unlink: false, dev: true })
+  } else {
+      print.info('Too bad it didn\'t work out between us! Don\'t forget to remove the hide code:\n\nimport SplashScreen from \'react-native-splash-screen\';\n\nSplashScreen.hide();');
+  }
 }
 
 // Required in all Ignite CLI plugins
